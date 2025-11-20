601014c5c86529afc1f0467a0f395c045aecd552
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2025-11-20 15:13:23 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
 module.exports = supabase;
